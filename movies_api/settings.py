7dee77382b852f6e7212b3cc98f762ca27a3c78d
Timur <tima@MacBook-Air-Timur.local>
2021-08-20 15:20:55 +0600

    static dir remove

--- movies_api/settings.py
+++ movies_api/settings.py
@@ -150,8 +150,6 @@ SIMPLE_JWT = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
