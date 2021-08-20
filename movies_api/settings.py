dda8c2778b74861cfb54ca023a8b1919dd117824
Timur <tima@MacBook-Air-Timur.local>
2021-08-20 15:35:16 +0600

    fixed static, deleted dir

--- movies_api/settings.py
+++ movies_api/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
